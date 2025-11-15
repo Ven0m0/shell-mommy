a154d2a7fbae792cc34889c6bd9d7602263d44e9
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-15 05:45:25 +0100

    Update shell-mommy.sh

--- shell-mommy.sh
+++ shell-mommy.sh
@@ -1,22 +1,21 @@
+#!/usr/bin/env bash
+LC_ALL=C LANG=C
 # sudofox/shell-mommy.sh
 
-mommy() (
-
+mommy()(
   # SHELL_MOMMYS_LITTLE - what to call you~ (default: "girl")
   # SHELL_MOMMYS_PRONOUNS - what pronouns mommy will use for themself~ (default: "her")
   # SHELL_MOMMYS_ROLES - what role mommy will have~ (default "mommy")
-
-  COLORS_LIGHT_PINK='\033[38;5;217m'  #\e[38;5;217m
-  COLORS_LIGHT_BLUE='\033[38;5;117m'  #'\e[38;5;117m
-  COLORS_FAINT='\033[2m'              #'\e[2m
-  COLORS_RESET='\033[0m'              #'\e[0m
+  COLORS_LIGHT_PINK='\033[[38;5;217m'  #\e[[38;5;217m
+  COLORS_LIGHT_BLUE='\033[[38;5;117m'  #'\e[[38;5;117m
+  COLORS_FAINT='\033[[2m'              #'\e[[2m
+  COLORS_RESET='\033[[0m'              #'\e[[0m
 
   DEF_WORDS_LITTLE="girl"
   DEF_WORDS_PRONOUNS="her"
   DEF_WORDS_ROLES="mommy"
   DEF_MOMMY_COLOR="${COLORS_LIGHT_PINK}"
-  DEF_ONLY_NEGATIVE="false"
-
+  DEF_ONLY_NEGATIVE="true"
   NEGATIVE_RESPONSES="do you need MOMMYS_ROLE's help~? ❤️
 Don't give up, my love~ ❤️
 Don't worry, MOMMYS_ROLE is here to help you~ ❤️
@@ -54,53 +53,29 @@ you did an amazing job, my dear~ ❤️
 you're such a smart cookie~ ❤️"
 
   # allow for overriding of default words (IF ANY SET)
-
-  if [ -n "$SHELL_MOMMYS_LITTLE" ]; then
-    DEF_WORDS_LITTLE="${SHELL_MOMMYS_LITTLE}"
-  fi
-  if [ -n "$SHELL_MOMMYS_PRONOUNS" ]; then
-    DEF_WORDS_PRONOUNS="${SHELL_MOMMYS_PRONOUNS}"
-  fi
-  if [ -n "$SHELL_MOMMYS_ROLES" ]; then
-    DEF_WORDS_ROLES="${SHELL_MOMMYS_ROLES}"
-  fi
-  if [ -n "$SHELL_MOMMYS_COLOR" ]; then
-    DEF_MOMMY_COLOR="${SHELL_MOMMYS_COLOR}"
-  fi
+  [[ -n "$SHELL_MOMMYS_LITTLE" ]] && DEF_WORDS_LITTLE="${SHELL_MOMMYS_LITTLE}"
+  [[ -n "$SHELL_MOMMYS_PRONOUNS" ]] && DEF_WORDS_PRONOUNS="${SHELL_MOMMYS_PRONOUNS}"
+  [[ -n "$SHELL_MOMMYS_ROLES" ]] && DEF_WORDS_ROLES="${SHELL_MOMMYS_ROLES}"
+  [[ -n "$SHELL_MOMMYS_COLOR" ]] && DEF_MOMMY_COLOR="${SHELL_MOMMYS_COLOR}"
   # allow overriding to true
-  if [ "$SHELL_MOMMYS_ONLY_NEGATIVE" = "true" ]; then
-    DEF_ONLY_NEGATIVE="true"
-  fi
+  [[ "$SHELL_MOMMYS_ONLY_NEGATIVE" = "true" ]] && DEF_ONLY_NEGATIVE="true"
   # if the variable is set for positive/negative responses, overwrite it
-  if [ -n "$SHELL_MOMMYS_POSITIVE_RESPONSES" ]; then
-    POSITIVE_RESPONSES="$SHELL_MOMMYS_POSITIVE_RESPONSES"
-  fi
-  if [ -n "$SHELL_MOMMYS_NEGATIVE_RESPONSES" ]; then
-    NEGATIVE_RESPONSES="$SHELL_MOMMYS_NEGATIVE_RESPONSES"
-  fi
-
+  [[ -n "$SHELL_MOMMYS_POSITIVE_RESPONSES" ]] && POSITIVE_RESPONSES="$SHELL_MOMMYS_POSITIVE_RESPONSES"
+  [[ -n "$SHELL_MOMMYS_NEGATIVE_RESPONSES" ]] && NEGATIVE_RESPONSES="$SHELL_MOMMYS_NEGATIVE_RESPONSES"
   # split a string on forward slashes and return a random element
-  pick_word() {
-    echo "$1" | tr '/' '\n' | shuf | sed 1q
-  }
-
-  pick_response() { # given a response type, pick an entry from the list
-
-    if [ "$1" = "positive" ]; then
+  pick_word(){ echo "$1" | tr '/' '\n' | shuf | sed 1q; }
+  pick_response(){ # given a response type, pick an entry from the list
+    if [[ "$1" = "positive" ]]; then
       element=$(echo "$POSITIVE_RESPONSES" | shuf | sed 1q)
-    elif [ "$1" = "negative" ]; then
+    elif [[ "$1" = "negative" ]]; then
       element=$(echo "$NEGATIVE_RESPONSES" | shuf | sed 1q)
     else
-      echo "Invalid response type: $1"
-      exit 1
+      echo "Invalid response type: $1"; exit 1
     fi
-
     # Return the selected response
     echo "$element"
-
   }
-
-  sub_terms() { # given a response, sub in the appropriate terms
+  sub_terms(){ # given a response, sub in the appropriate terms
     response="$1"
     # pick_word for each term
     affectionate_term="$(pick_word "${DEF_WORDS_LITTLE}")"
@@ -114,20 +89,17 @@ you're such a smart cookie~ ❤️"
     # print faint and colorcode
     printf "${DEF_MOMMY_COLOR}$response${COLORS_RESET}\n"
   }
-
-  success() {
+  success(){
     (
       # if we're only supposed to show negative responses, return
-      if [ "$DEF_ONLY_NEGATIVE" = "true" ]; then
-        return 0
-      fi
+      [[ "$DEF_ONLY_NEGATIVE" = "true" ]] && return 0
       # pick_response for the response type
       response="$(pick_response "positive")"
       sub_terms "$response" >&2
     )
     return 0
   }
-  failure() {
+  failure(){
     rc=$?
     (
       response="$(pick_response "negative")"
@@ -136,7 +108,6 @@ you're such a smart cookie~ ❤️"
     return $rc
   }
   # eval is used here to allow for alias resolution
-
   # TODO: add a way to check if we're running from PROMPT_COMMAND to use the previous exit code instead of doing things this way
   eval "$@" && success || failure
   return $?
